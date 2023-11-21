fcf812602cdfe29d979e82943d1f4961532b4d6f
Antônio Lucas <129812080+Tony38700@users.noreply.github.com>
2023-11-21 10:34:33 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -11,4 +11,13 @@ public class Professor extends Pessoa{
         this.dataInicio = dataInicio;
         this.disciplinaLecionada = disciplinaLecionada;
     }
+    
+    @Override
+    public void exibirInformacoes()
+    {
+        System.out.println("Professor: " + this.nome);
+        System.out.println("ID do professor: " + this.identificacao);
+        System.out.println("Data de contratacao do professor: " + this.dataInicio);
+        System.out.println("Disciplina lecionada pelo professor: " + this.disciplinaLecionada);
+    }
 }
