c36d6057051084900a9382b81de5c9c17aacb562
Antônio Lucas <129812080+Tony38700@users.noreply.github.com>
2023-11-21 10:34:46 -0300

    Update Diretor.java

--- Diretor.java
+++ Diretor.java
@@ -8,4 +8,12 @@ public class Diretor extends Pessoa{
         this.identificacao = identificacao;
         this.dataInicio = dataInicio;
     }
+    
+    @Override
+    public void exibirInformacoes()
+    {
+        System.out.println("Diretor: " + this.nome);
+        System.out.println("ID do diretor: " + this.identificacao);
+        System.out.println("Data de contratacao do diretor: " + this.dataInicio);
+    }
 }
